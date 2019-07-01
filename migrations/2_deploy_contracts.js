22f0c10a91611f6e75d6c9c70753f34bd3b5fadb
Carter Brainerd <0xCB@protonmail.com>
2019-07-01 19:31:25 +0300

    Finish token and crowdsale contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
-var Oort = artifacts.require("./Oort.sol");
+var OortToken = artifacts.require("./OortToken.sol");
+var OortTokenSale = artifacts.require("./OortTokenSale.sol");
+
 
 module.exports = function(deployer) {
-  deployer.deploy(Oort);
+  deployer.deploy(OortToken, 1000000000).then(function() { // Start with 1B tokens
+    // Token price is 0.001 ETH
+    var tokenPrice = 1000000000000000;
+    return deployer.deploy(OortTokenSale, OortToken.address, tokenPrice)
+  })
 };
